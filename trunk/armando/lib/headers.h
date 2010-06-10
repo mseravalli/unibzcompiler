79b11cfd26d7955abed31e330c6464c62df11ba9
chopinx04 <chopinx04@3b1cde2d-8205-3936-bb42-63b420203858>
2010-06-10 00:13:44 +0000

    ho cercato di integrare la symbol table in una grammatica semplificata ma con scarso successo. Devo guardare perche' non compila

--- trunk/armando/lib/headers.h
+++ trunk/armando/lib/headers.h
@@ -10,4 +10,19 @@
  *                                                                            *
  ******************************************************************************/
 
+typedef struct char_node {
+    char a;                 // character of the string (EOS == '\0')
+    struct char_node *next; // pointer to the next character
+} char_node;
 
+typedef struct sym_node {
+    struct sym_node*    next;
+    int                 token;
+    char_node           *lexeme;
+    int                 type;       // 0 == ganz
+                                    // 1 == genau
+} sym_node;
+
+/******************************************************************************/
+sym_node *add_symbol(int token, char *lexeme, int type);
+void print_symbols();
---
+++ trunk/armando/yacc/example.y
@@ -0,0 +1,32 @@
+%{
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include "../lex/lex.example.c"
+#include "../lib/headers.h"
+%}
+
+%union
+{
+    char* lexeme;
+    int   <token>
+}
+
+%token <token> ID
+%start Scope
+%%
+
+Scope               : Program               {print_table();}
+                    ;
+
+Program             : Program ID            {add_symbol($2, yylval.lexeme, 0);}
+                    | /* empty string */
+                    ;
+
+%%
+
+void yyerror (char *s)
+{
+    fprintf (stderr, "%s\n", s);
+}
