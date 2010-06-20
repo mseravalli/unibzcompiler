b0f9e396969494c7b0656700359ad024689bd626
chopinx04 <chopinx04@3b1cde2d-8205-3936-bb42-63b420203858>
2010-06-20 11:57:03 +0000

    demo del funzionamento dello scop sul main

--- project/lib/headers.h
+++ project/lib/headers.h
@@ -10,30 +10,39 @@
  *                                                                            *
  ******************************************************************************/
 
-#define DEFAULT_IVAL 0
-#define DEFAULT_FVAL 0.0f
-
+/* each character of a lexeme till and of string */
 typedef struct char_node {
     char a;                 // character of the string (EOS == '\0')
     struct char_node *next; // pointer to the next character
 } char_node;
 
+/* node of a symbol table */
 typedef struct sym_node {
     struct sym_node*    next;
-    int                 token;
     char_node           *lexeme;
+    int                 token;
+    int                 line;
     char                type;
+    struct scope        *nscope;    // new scope of the encountered scope
 } sym_node;
 
+/*
+ * structure that identifies a scope by its name, stores
+ * the symbol node in which the scope is placed and the
+ * new symbol table.
+ */ 
+typedef struct scope {
+    sym_node        *parent;
+    char_node       *lexeme;
+    sym_node        *symtbl;
+} scope;
+
 /******************************************************************************/
-sym_node *add_symbol(int token, char *lexeme, char type);
-int modify_symbol(char* lexeme, char* v);
-int isCorrectType(char a, char b);
-int find_symbol(char *lexeme);
-sym_node* getSymNode(int position);
-void print_symbols();
-char* bool_compare (char* a, char* b, char op);
-char* num_compare (char* a, char* b, char op);
-char* calculate (char* a, char* b, char op);
-char* itoa (int);
-char* ftoa (float);
+scope       *init_scope(sym_node *parent, char_node *lexeme);
+sym_node    *add_symbol(int token, char *lexeme, int line,int type, scope* nscope);
+//int         isCorrectType(char a, char b);
+int         find_symbol(char *lexeme);
+//sym_node    *get_node(int position);
+void        print_symbols();
+//char*       bool_compare(char* a, char* b, char op);
+//char*       num_compare(char* a, char* b, char op);
