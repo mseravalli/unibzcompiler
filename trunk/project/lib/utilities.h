57e6f0233ea8fa1e50ba6cb824169f07f7ecba3d
chopinX04 <chopinX04@3b1cde2d-8205-3936-bb42-63b420203858>
2010-06-20 20:33:59 +0000

    aggiunto un commento e rimosso print_all (non ne vale la pena)

--- trunk/project/lib/utilities.h
+++ trunk/project/lib/utilities.h
@@ -1,1 +1,13 @@
+/****************************************************************************** 
+ *                                                                            *
+ * file name: headers.h                                                       *
+ * author: Armando Miraglia                                                   *
+ * email: armando.miraglia@stud-inf.unibz.it                                  *
+ * version: 0.1                                                               *
+ *                                                                            *
+ * last modification: 20/06/10 15:56                                          *
+ * last mod. author: Armando                                                  *
+ *                                                                            *
+ ******************************************************************************/
+
 int yyline = 1;             // used for reporting the current line number
