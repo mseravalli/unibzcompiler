77a35339a4402903d96ca0574b3d2e3428b3c5ed
chopinx04 <chopinx04@3b1cde2d-8205-3936-bb42-63b420203858>
2010-06-20 14:38:51 +0000

    ora la symbol table funziona.. cerco di integrare qualcosa con la grammatica

--- project/lib/headers.h
+++ project/lib/headers.h
@@ -5,7 +5,7 @@
  * email: armando.miraglia@stud-inf.unibz.it                                  *
  * version: 0.1                                                               *
  *                                                                            *
- * last modification: 28/05/10 10:36                                          *
+ * last modification: 20/06/10 15:56                                          *
  * last mod. author: Armando                                                  *
  *                                                                            *
  ******************************************************************************/
@@ -22,7 +22,7 @@ typedef struct sym_node {
     char_node           *lexeme;
     int                 token;
     int                 line;
-    char                type;
+    int                 type;
     struct scope        *nscope;    // new scope of the encountered scope
 } sym_node;
 
@@ -32,19 +32,21 @@ typedef struct sym_node {
  * new symbol table.
  */ 
 typedef struct scope {
-    sym_node        *parent;
+    struct scope    *parent;
     char_node       *lexeme;
     sym_node        *symtbl;
 } scope;
 
 /******************************************************************************/
-scope       *init_scope(sym_node *parent, char_node *lexeme);
+scope       *init_scope(scope *parent, char_node *lexeme);
 int         exit_scope();
 char_node   *generate_scope_name();
-sym_node    *add_symbol(int token, char *lexeme, int line,int type, scope* nscope);
+sym_node    *add_symbol(int token, char *lexeme, int line,int type, int is_scope);
 //int         isCorrectType(char a, char b);
 int         find_symbol(char *lexeme);
 //sym_node    *get_node(int position);
+void        print_all();
 void        print_symbols();
+void        print_scope();
 //char*       bool_compare(char* a, char* b, char op);
 //char*       num_compare(char* a, char* b, char op);
